7e80ea8d2c507aeec2f12e630364b591f023691e
Leong Peck Yoke <Leong_Peck_Yoke@imda.gov.sg>
2023-08-04 15:49:26 +0800

    new comment to generate algorithm component

--- ai-verify-plugin/src/algorithms.mjs
+++ ai-verify-plugin/src/algorithms.mjs
@@ -1,8 +1,12 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { spawn } from 'node:child_process';
+import { chdir, exit } from 'node:process';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 import { validate } from 'jsonschema';
 // import {bundleMDX} from 'mdx-bundler';
-import { readJSON, srcDir, copyAndRenameTemplates } from './utils.mjs';
+import { readJSON, srcDir, rootDir } from './utils.mjs';
 
 import { algorithmSchema } from './schemas.mjs';
 
@@ -67,4 +71,89 @@ export async function validateAllAlgorithms(argv) {
   }
 
   return true;
+}
+
+
+export function generateAlgorithm(argv) {
+  const COOKIECUTTER = process.env.COOKIECUTTER || "cookiecutter";
+
+  console.log("Generating skeleton algorithm..")
+
+  // create director 
+  // const pluginDir = argv.pluginDir;
+  // const pluginDir = path.resolve(".");
+  const pluginDir = argv._pluginDir;
+  
+  if (!fs.existsSync(pluginDir)) {
+    console.error("Plugin directory does not exists")
+    process.exit(-1);
+  }
+
+  // create widget dir
+  const algoDir = path.join(pluginDir, ALGORITHM_SUBDIR)
+  if (!fs.existsSync(algoDir)) {
+    fs.mkdirSync(algoDir);
+  }
+
+  // check algo component directory does not exists
+  const compDir = path.join(algoDir, argv.cid);
+  if (fs.existsSync(compDir)) {
+    console.error(`Algorithm component ${argv.cid} already exists`);
+    exit(-1);
+  }
+
+  const templateDir = path.join(rootDir, "../ai-verify-algorithm-template");
+  const configFile = path.join(algoDir, `${argv.cid}-config.yaml`);
+
+  const cleanupFiles = () => {
+    if (fs.existsSync(configFile)) {
+      fs.unlinkSync(configFile);
+    }
+    const licenseFile = path.join(compDir, "LICENSE");
+    if (fs.existsSync(licenseFile))
+      fs.unlinkSync(licenseFile)
+    const pluginMetaFile = path.join(compDir, "plugin.meta.json");
+    if (fs.existsSync(pluginMetaFile))
+      fs.unlinkSync(pluginMetaFile)
+  }
+
+  try {
+    chdir(algoDir);
+
+    // create config file for cookie cutter
+    fs.writeFileSync(configFile, `
+default_context:
+  author: "${argv.author}"
+  plugin_name: "${argv.name}"
+  project_slug: "${argv.cid}"
+  plugin_version: "${argv.pluginVersion}"
+  plugin_description: "${argv.description || ""}"
+  algo_model_support: "${argv.modelSupport}"
+  require_ground_truth: "${argv.requireGroundTruth?"True":"False"}"
+`)
+
+    const args = ['--config-file', configFile, templateDir];
+    if (!argv.interactive) {
+      args.unshift('--no-input')
+    }
+
+    const rl = readline.createInterface({ input, output });
+    const cc = spawn(COOKIECUTTER,args);
+    cc.stdout.on('data', async data => {
+      const qn = data.toString();
+      if (qn.includes("license")) {
+        cc.stdin.write("\n")  
+      } else {
+        const ans = await rl.question(qn);
+        cc.stdin.write(ans + "\n")  
+      }
+    })
+    cc.on('close', code => {
+      rl.close();
+      cleanupFiles();
+    })
+  } catch (e) {
+    console.error("Error executing cookiecutter", e.stdout.toString())
+    cleanupFiles();
+  }
 }

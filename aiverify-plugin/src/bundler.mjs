2a6234682fae2c832e23470434e55a5f03ed0c40
Leong Peck Yoke <leong_peck_yoke@imda.gov.sg>
2025-06-17 14:20:54 +0800

    update options for mdx-bundler

--- aiverify-plugin/src/bundler.mjs
+++ aiverify-plugin/src/bundler.mjs
@@ -1,32 +1,39 @@
-import {bundleMDX} from 'mdx-bundler';
-import remarkMdxImages from 'remark-mdx-images';
-import remarkGfm from 'remark-gfm';
-import { getPluginDir } from './pluginManager.mjs';
+import { bundleMDX } from "mdx-bundler";
+import rehypeMdxImportMedia from "rehype-mdx-import-media";
+import remarkGfm from "remark-gfm";
+import { getPluginDir } from "./pluginManager.mjs";
 
-export async function getMdxWidgetBundle (mdxPath) {
-	try {
-		const result = await bundleMDX({
-			cwd: getPluginDir(),
-			file: mdxPath,
-			mdxOptions: options => {
-				options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkMdxImages, remarkGfm]
-				return options
-			},
-			esbuildOptions: options => {
-				options.loader = {
-					...options.loader,
-					'.png': 'dataurl',
-				}
-				return options
-			},
-		})
-		return {
-			result
-		};	
-	} catch (e) {
-		console.error("Error loading MDX", mdxPath, e);
-		return {
-			error: e,
-		};
-	}
+export async function getMdxWidgetBundle(mdxPath) {
+  try {
+    const result = await bundleMDX({
+      cwd: getPluginDir(),
+      file: mdxPath,
+      mdxOptions: (options) => {
+        options.remarkPlugins = [
+          ...(options.remarkPlugins ?? []),
+          remarkGfm,
+        ];
+		options.rehypePlugins = [
+			...(options.rehypePlugins ?? []),
+			rehypeMdxImportMedia,
+		];
+		return options;
+      },
+      esbuildOptions: (options) => {
+        options.loader = {
+          ...options.loader,
+          ".png": "dataurl",
+        };
+        return options;
+      },
+    });
+    return {
+      result,
+    };
+  } catch (e) {
+    console.error("Error loading MDX", mdxPath, e);
+    return {
+      error: e,
+    };
+  }
 }

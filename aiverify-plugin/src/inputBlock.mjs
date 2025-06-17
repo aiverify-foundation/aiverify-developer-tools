4a215fb1662a3292d835fe298b9e58c115f31a61
Leong Peck Yoke <leong_peck_yoke@imda.gov.sg>
2025-06-17 17:08:47 +0800

    use  method from  to validate MDX

--- aiverify-plugin/src/inputBlock.mjs
+++ aiverify-plugin/src/inputBlock.mjs
@@ -1,57 +1,63 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { validate } from 'jsonschema';
+import fs from "node:fs";
+import path from "node:path";
+import { validate } from "jsonschema";
 // import {bundleMDX} from 'mdx-bundler';
-import { readJSON, srcDir, copyAndRenameTemplates } from './utils.mjs';
+import { readJSON, srcDir, copyAndRenameTemplates } from "./utils.mjs";
 
-import { inputBlockSchema } from './schemas.mjs';
+import { inputBlockSchema } from "./schemas.mjs";
 
 const INPUT_BLOCK_SUBDIR = "inputs";
 
 export function generateInputBock(argv) {
-  console.log("Generating skeleton input block..")
+  console.log("Generating skeleton input block..");
 
-  // create director 
+  // create director
   // const pluginDir = argv.pluginDir;
   // const pluginDir = path.resolve(".");
   const pluginDir = argv._pluginDir;
-  
+
   if (!fs.existsSync(pluginDir)) {
-    console.error("Plugin directory does not exists")
+    console.error("Plugin directory does not exists");
     process.exit(-1);
   }
 
   // create widget dir
-  const inputBlockDir = path.join(pluginDir, INPUT_BLOCK_SUBDIR)
+  const inputBlockDir = path.join(pluginDir, INPUT_BLOCK_SUBDIR);
   if (!fs.existsSync(inputBlockDir)) {
     fs.mkdirSync(inputBlockDir);
   }
 
   // create meta file
-  const metaFile = path.join(inputBlockDir, `${argv.cid}.meta.json`)
+  const metaFile = path.join(inputBlockDir, `${argv.cid}.meta.json`);
   let meta = {
     cid: argv.cid,
   };
   if (fs.existsSync(metaFile)) {
     meta = readJSON(metaFile);
   }
-  for (let key of ["name","description","author","version","tag","group","width","fullScreen"]) {
+  for (let key of [
+    "name",
+    "description",
+    "author",
+    "version",
+    "tag",
+    "group",
+    "width",
+    "fullScreen",
+  ]) {
     if (key in argv) {
       switch (key) {
-        case 'tag':
-          if (!meta['tags'] || argv.force)
-            meta['tags'] = argv[key];
+        case "tag":
+          if (!meta["tags"] || argv.force) meta["tags"] = argv[key];
           break;
         default:
-          if (!meta[key] || argv.force)
-            meta[key] = argv[key];
+          if (!meta[key] || argv.force) meta[key] = argv[key];
           break;
       }
     }
   }
-  if (!meta.name)
-    meta.name = meta.cid;
-  fs.writeFileSync(metaFile, JSON.stringify(meta, null, 2))
+  if (!meta.name) meta.name = meta.cid;
+  fs.writeFileSync(metaFile, JSON.stringify(meta, null, 2));
 
   // create sample MDX file
   // const mdxFile = path.join(inputBlockDir, `${argv.cid}.mdx`);
@@ -61,83 +67,73 @@ export function generateInputBock(argv) {
   // const summaryFile = path.join(inputBlockDir, `${argv.cid}.ts`);
   // fs.writeFileSync(summaryFile, SUMMARY_TEMPLATE.trim() + "\n")
 
-  copyAndRenameTemplates(path.join(srcDir,"inputs"), "sample-input-block", inputBlockDir, argv.cid)
+  copyAndRenameTemplates(
+    path.join(srcDir, "inputs"),
+    "sample-input-block",
+    inputBlockDir,
+    argv.cid
+  );
 }
 
-export async function validateInputBlock(argv, meta, skipMDXValidation=false) {
+export async function validateInputBlock(
+  argv,
+  meta,
+  skipMDXValidation = false
+) {
   const pluginDir = argv._pluginDir;
 
-  const inputBlockDir = path.join(pluginDir, INPUT_BLOCK_SUBDIR)
+  const inputBlockDir = path.join(pluginDir, INPUT_BLOCK_SUBDIR);
 
   // validate meta
   try {
     const res = validate(meta, inputBlockSchema);
     if (!res.valid) {
-      console.error(`Input block meta for ${meta.cid} validation errors:`, res.errors);
+      console.error(
+        `Input block meta for ${meta.cid} validation errors:`,
+        res.errors
+      );
       return false;
     }
   } catch (err) {
-    console.error(`Input block meta for ${meta.cid} is invalid`, err)
+    console.error(`Input block meta for ${meta.cid} is invalid`, err);
     return false;
   }
 
-  const summaryFile = path.join(inputBlockDir, `${meta.cid}.summary.mdx`)
+  const summaryFile = path.join(inputBlockDir, `${meta.cid}.summary.mdx`);
   if (!fs.existsSync(summaryFile)) {
-    console.log(`Summary file ${meta.cid}.summary.mdx is missing`)
+    console.log(`Summary file ${meta.cid}.summary.mdx is missing`);
     return false;
   }
 
-  const mdxFile = path.join(inputBlockDir, `${meta.cid}.mdx`)
+  const mdxFile = path.join(inputBlockDir, `${meta.cid}.mdx`);
   if (!fs.existsSync(mdxFile)) {
-    console.log(`Widget MDX ${meta.cid}.mdx is missing`)
+    console.log(`Widget MDX ${meta.cid}.mdx is missing`);
     return false;
   }
-  
+
   // validate mdx
   if (!skipMDXValidation) {
-    // validate widget mdx
-    const {bundleMDX} = await import('mdx-bundler');
-    const remarkMdxImages = await import('remark-mdx-images');
-    const remarkGfm = await import('remark-gfm');
+    // validate input block mdx
+    const { getMdxWidgetBundle } = await import("./bundler.mjs");
     try {
-      await bundleMDX({
-        cwd: pluginDir,
-        file: mdxFile,
-        mdxOptions: options => {
-          options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkMdxImages, remarkGfm]
-          return options
-        },
-        esbuildOptions: options => {
-          options.loader = {
-            ...options.loader,
-            '.png': 'dataurl',
-          }
-          return options
-        },
-      })
-    } catch(e) {
+      const result1 = getMdxWidgetBundle(mdxFile)
+      if (!result1) {
+        console.log("MDX error");
+        return false;
+      }
+    } catch (e) {
       console.log("MDX error", e);
       return false;
     }
 
     // validate widget mdx
     try {
-      await bundleMDX({
-        cwd: pluginDir,
-        file: summaryFile,
-        mdxOptions: options => {
-          options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkMdxImages, remarkGfm]
-          return options
-        },
-        esbuildOptions: options => {
-          options.loader = {
-            ...options.loader,
-            '.png': 'dataurl',
-          }
-          return options
-        },
-      })
-    } catch(e) {
+      const result2 = getMdxWidgetBundle(summaryFile)
+      if (!result2) {
+        console.log("MDX error");
+        return false;
+      }
+    } catch (e) {
       console.log("MDX error", e);
       return false;
     }
@@ -148,27 +144,25 @@ export async function validateInputBlock(argv, meta, skipMDXValidation=false) {
 
 export async function validateAllInputBlocks(argv) {
   const pluginDir = argv._pluginDir;
-  
+
   // const widgetDir = path.join(argv.pluginDir, WIDGET_SUBDIR)
-  const inputBlockDir = path.join(pluginDir, INPUT_BLOCK_SUBDIR)
+  const inputBlockDir = path.join(pluginDir, INPUT_BLOCK_SUBDIR);
   if (!fs.existsSync(inputBlockDir)) {
-    console.log("inputs directory is missing")
+    console.log("inputs directory is missing");
     return true;
   }
 
   const myfiles = fs.readdirSync(inputBlockDir);
   for (let f of myfiles) {
-    if (!f.endsWith(".meta.json"))
-      continue;
+    if (!f.endsWith(".meta.json")) continue;
     // console.log("validating", f);
     const meta = readJSON(path.join(inputBlockDir, f));
     if (!meta.cid) {
       console.error("Invalid input block meta file", f);
       return false;
-    }  
-    if (!(await validateInputBlock(argv, meta)))
-      return false;
-}
+    }
+    if (!(await validateInputBlock(argv, meta))) return false;
+  }
 
   return true;
 }
--- aiverify-plugin/src/reportWidget.mjs
+++ aiverify-plugin/src/reportWidget.mjs
@@ -1,18 +1,17 @@
-import fs from 'node:fs';
-import path from 'node:path';
-import { validate } from 'jsonschema';
-// import {bundleMDX} from 'mdx-bundler';
+import fs from "node:fs";
+import path from "node:path";
+import { validate } from "jsonschema";
 
-import { srcDir, readJSON, copyAndRenameTemplates } from './utils.mjs';
+import { srcDir, readJSON, copyAndRenameTemplates } from "./utils.mjs";
 
-import { reportWidgetSchema } from './schemas.mjs';
+import { reportWidgetSchema } from "./schemas.mjs";
 
 const WIDGET_SUBDIR = "widgets";
 
 export function generateWidget(argv) {
-  console.log(`Generating skeleton report widget for ${argv.cid}..`)
+  console.log(`Generating skeleton report widget for ${argv.cid}..`);
 
-  // create director 
+  // create director
   // const pluginDir = argv.pluginDir;
   // if (!fs.existsSync(pluginDir)) {
   //   console.error("Plugin directory does not exists")
@@ -22,7 +21,7 @@ export function generateWidget(argv) {
   const pluginDir = argv._pluginDir;
 
   // create widget dir
-  const widgetDir = path.join(pluginDir, WIDGET_SUBDIR)
+  const widgetDir = path.join(pluginDir, WIDGET_SUBDIR);
   if (!fs.existsSync(widgetDir)) {
     fs.mkdirSync(widgetDir);
   }
@@ -36,30 +35,38 @@ export function generateWidget(argv) {
   if (fs.existsSync(metaFile)) {
     meta = readJSON(metaFile);
   }
-  for (let key of ["name","description","version","author","tag","minW","minH","maxW","maxH","dynamicHeight"]) {
+  for (let key of [
+    "name",
+    "description",
+    "version",
+    "author",
+    "tag",
+    "minW",
+    "minH",
+    "maxW",
+    "maxH",
+    "dynamicHeight",
+  ]) {
     if (key in argv) {
       switch (key) {
-        case 'minW':
-        case 'minH':
-        case 'maxW':
-        case 'maxH':
+        case "minW":
+        case "minH":
+        case "maxW":
+        case "maxH":
           if (!meta.widgetSize[key] || argv.force)
             meta.widgetSize[key] = argv[key];
           break;
-        case 'tag':
-          if (!meta['tags'] || argv.force)
-            meta['tags'] = argv[key];
+        case "tag":
+          if (!meta["tags"] || argv.force) meta["tags"] = argv[key];
           break;
         default:
-          if (!meta[key] || argv.force)
-            meta[key] = argv[key];
+          if (!meta[key] || argv.force) meta[key] = argv[key];
           break;
-        }
+      }
     }
   }
 
-  if (!meta.name)
-    meta.name = meta.cid;
+  if (!meta.name) meta.name = meta.cid;
 
   if (argv._dep && (!meta.dependencies || argv.force)) {
     let dependencies = [];
@@ -70,20 +77,20 @@ export function generateWidget(argv) {
       const cid = dep[1];
       let obj = {
         cid,
-      }
+      };
       const count = dep.length;
       if (count >= 3) {
-        obj['gid'] = dep[2];
+        obj["gid"] = dep[2];
       }
       if (count == 4) {
-        obj['version'] = dep[3];
+        obj["version"] = dep[3];
       }
-      dependencies.push(obj)
+      dependencies.push(obj);
       mockdata.push({
         type: dep[0],
         cid,
         datapath,
-      })
+      });
       let datapathJoined = path.join(widgetDir, datapath);
       if (!fs.existsSync(datapathJoined)) {
         fs.writeFileSync(datapathJoined, "{}");
@@ -100,24 +107,29 @@ export function generateWidget(argv) {
         key: prop[0],
         helper: prop[1],
         default: prop[2],
-      })
+      });
     }
     meta.properties = properties;
   }
 
-  fs.writeFileSync(metaFile, JSON.stringify(meta, null, 2))
+  fs.writeFileSync(metaFile, JSON.stringify(meta, null, 2));
 
   // create sample MDX file
   // const mdxFile = path.join(widgetDir, `${argv.cid}.mdx`);
   // fs.writeFileSync(mdxFile, MDX_TEMPLATE + "\n")
   // fs.cpSync(path.join(srcDir,"widgets"), widgetDir, { recursive: true });
-  copyAndRenameTemplates(path.join(srcDir,"widgets"), "sample-widget", widgetDir, argv.cid)
+  copyAndRenameTemplates(
+    path.join(srcDir, "widgets"),
+    "sample-widget",
+    widgetDir,
+    argv.cid
+  );
 }
 
-export async function validateWidget(argv, meta, skipMDXValidation=false) {
+export async function validateWidget(argv, meta, skipMDXValidation = false) {
   const pluginDir = argv._pluginDir;
 
-  const widgetDir = path.join(pluginDir, WIDGET_SUBDIR)
+  const widgetDir = path.join(pluginDir, WIDGET_SUBDIR);
   // if (!fs.existsSync(widgetDir)) {
   //   console.log("widgets directory is missing")
   //   return false;
@@ -126,45 +138,36 @@ export async function validateWidget(argv, meta, skipMDXValidation=false) {
   try {
     const res = validate(meta, reportWidgetSchema);
     if (!res.valid) {
-      console.error(`Widget meta for ${meta.cid} has validation errors:`, res.errors);
+      console.error(
+        `Widget meta for ${meta.cid} has validation errors:`,
+        res.errors
+      );
       return false;
     }
   } catch (err) {
-    console.error(`Widget meta for ${meta.cid} is invalid`, err)
+    console.error(`Widget meta for ${meta.cid} is invalid`, err);
     return false;
   }
 
-  const mdxFile = path.join(widgetDir, `${meta.cid}.mdx`)
+  const mdxFile = path.join(widgetDir, `${meta.cid}.mdx`);
   if (!fs.existsSync(mdxFile)) {
-    console.error(`Widget MDX ${meta.cid}.mdx is missing`)
+    console.error(`Widget MDX ${meta.cid}.mdx is missing`);
     return false;
   }
-  
+
   // validate mdx
   if (!skipMDXValidation) {
-    const {bundleMDX} = await import('mdx-bundler');
-    const remarkMdxImages = await import('remark-mdx-images');
-    const remarkGfm = await import('remark-gfm');
+    const { getMdxWidgetBundle } = await import("./bundler.mjs");
     try {
-      await bundleMDX({
-        cwd: pluginDir,
-        file: mdxFile,
-        mdxOptions: options => {
-          options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkMdxImages, remarkGfm]
-          return options
-        },
-        esbuildOptions: options => {
-          options.loader = {
-            ...options.loader,
-            '.png': 'dataurl',
-          }
-          return options
-        },
-      })
-    } catch(e) {
+      const result = await getMdxWidgetBundle(mdxFile);
+      if (!result) {
+        console.log("MDX error")
+        return false
+      }
+    } catch (e) {
       console.log("MDX error", e);
       return false;
-    }  
+    }
   }
 
   return true;
@@ -172,26 +175,24 @@ export async function validateWidget(argv, meta, skipMDXValidation=false) {
 
 export async function validateAllWidgets(argv) {
   const pluginDir = argv._pluginDir;
-  
+
   // const widgetDir = path.join(argv.pluginDir, WIDGET_SUBDIR)
-  const widgetDir = path.join(pluginDir, WIDGET_SUBDIR)
+  const widgetDir = path.join(pluginDir, WIDGET_SUBDIR);
   if (!fs.existsSync(widgetDir)) {
-    console.log("widgets directory is missing")
+    console.log("widgets directory is missing");
     return true;
   }
 
   const myfiles = fs.readdirSync(widgetDir);
   for (let f of myfiles) {
-    if (!f.endsWith(".meta.json"))
-      continue;
+    if (!f.endsWith(".meta.json")) continue;
     // console.log("validating", f);
     const meta = readJSON(path.join(widgetDir, f));
     if (!meta.cid) {
       console.error("Invalid widget meta file", f);
       return false;
-    }  
-    if (!(await validateWidget(argv, meta)))
-      return false;
+    }
+    if (!(await validateWidget(argv, meta))) return false;
   }
 
   return true;

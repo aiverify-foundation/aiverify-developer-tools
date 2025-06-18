f0353a9d4a2f5381cc76b8683b117f09d1d4a6a7
Leong Peck Yoke <leong_peck_yoke@imda.gov.sg>
2025-06-18 15:11:14 +0800

    use test files from github

--- aiverify-algorithm-template/{{cookiecutter.project_slug}}/tests/e2e/test_e2e.py
+++ aiverify-algorithm-template/{{cookiecutter.project_slug}}/tests/e2e/test_e2e.py
@@ -6,22 +6,22 @@ from aiverify_test_engine.plugins.enums.model_type import ModelType
 
 {% if cookiecutter.algo_model_support == "Both" %}
 regression_non_pipeline = {
-    "data_path": str("tests/user_defined_files/data/sample_reg_donation_data.sav"),
+    "data_path": str("https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_reg_donation_data.sav"),
     "model_path": str(
-        "tests/user_defined_files/model/sample_reg_donation_sklearn_linear.LinearRegression.sav"
+        "https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/model/sample_reg_donation_sklearn_linear.LinearRegression.sav"
     ),
-    "ground_truth_path": str("tests/user_defined_files/data/sample_reg_donation_data.sav"),
+    "ground_truth_path": str("https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_reg_donation_data.sav"),
     "run_pipeline": False,
     "model_type": ModelType.REGRESSION,
     "ground_truth": "donation",
 }
 binary_classification_pipeline = {
     "data_path": str(
-        "tests/user_defined_files/data/sample_bc_pipeline_credit_data.sav"
+        "https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_bc_pipeline_credit_data.sav"
     ),
-    "model_path": str("tests/user_defined_files/model/sample_bc_credit_sklearn_linear.LogisticRegression.sav"),
+    "model_path": str("https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/model/sample_bc_credit_sklearn_linear.LogisticRegression.sav"),
     "ground_truth_path": str(
-        "tests/user_defined_files/data/sample_bc_pipeline_credit_ytest_data.sav"
+        "https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_bc_pipeline_credit_ytest_data.sav"
     ),
     "run_pipeline": False,
     "model_type": ModelType.CLASSIFICATION,
@@ -29,11 +29,11 @@ binary_classification_pipeline = {
 }
 {% elif cookiecutter.algo_model_support == "Regression" %}
 regression_non_pipeline = {
-    "data_path": str("tests/user_defined_files/data/sample_reg_donation_data.sav"),
+    "data_path": str("https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_reg_donation_data.sav"),
     "model_path": str(
-        "tests/user_defined_files/model/sample_reg_donation_sklearn_linear.LinearRegression.sav"
+        "https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/model/sample_reg_donation_sklearn_linear.LinearRegression.sav"
     ),
-    "ground_truth_path": str("tests/user_defined_files/data/sample_reg_donation_data.sav"),
+    "ground_truth_path": str("https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_reg_donation_data.sav"),
     "run_pipeline": False,
     "model_type": ModelType.REGRESSION,
     "ground_truth": "donation",
@@ -41,11 +41,11 @@ regression_non_pipeline = {
 {% elif cookiecutter.algo_model_support == "Classification" %}
 binary_classification_pipeline = {
     "data_path": str(
-        "tests/user_defined_files/data/sample_bc_pipeline_credit_data.sav"
+        "https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_bc_pipeline_credit_data.sav"
     ),
-    "model_path": str("tests/user_defined_files/model/sample_bc_credit_sklearn_linear.LogisticRegression.sav"),
+    "model_path": str("https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/model/sample_bc_credit_sklearn_linear.LogisticRegression.sav"),
     "ground_truth_path": str(
-        "tests/user_defined_files/data/sample_bc_pipeline_credit_ytest_data.sav"
+        "https://github.com/aiverify-foundation/aiverify/raw/refs/heads/main/stock-plugins/user_defined_files/data/sample_bc_pipeline_credit_ytest_data.sav"
     ),
     "run_pipeline": False,
     "model_type": ModelType.CLASSIFICATION,
